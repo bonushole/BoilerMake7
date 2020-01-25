c509e7cba2bb0718a0b7d2988759ccbad559f207
Maximilian Evans <156max@gmail.com)>
2020-01-25 18:44:10 -0500

    real q name convo thing

--- src/slot.js
+++ src/slot.js
@@ -1,20 +1,51 @@
 import React from 'react';
 import './style/slot.scss';
+import './style/slotSelected.scss';
 
 export class Slot extends React.Component{
 
     constructor(props, game){
        super(props);
+       this.game = game;
+       this.onClick = this.onClick.bind(this);
+       this.toggled = false;
+    
+    }
+    
+    onClick(){
+    
+        console.log("clicked");
+        this.game.setToggled(this);
+    
+    }
+    
+    toggle() {
+    
+        this.toggled = true;
+        //this.forceUpdate();
+    
+    }
+    
+    untoggle() {
+    
+        this.toggled = false;
     
     }
 
     render() {
     
     //style={{float: "left"}}
-        return( 
-            <div className='slot' style={{float: "left"}}> slot </div>
-            
-        );
+        if(this.toggled){
+            return( 
+                <div className='slotSelected' onClick={this.onClick} style={{float: "left"}}> slot </div>
+                
+            );
+        }else{
+            return( 
+                <div className='slot' onClick={this.onClick} style={{float: "left"}}> slot </div>
+                
+            );
+        }
     
     }
 }
