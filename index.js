48f5093161e81408292c517ab7f15ff6839c49df
Maximilian Evans <156max@gmail.com)>
2020-01-25 13:03:55 -0500

    messing w/ react

--- index.js
+++ index.js
@@ -1,17 +1,29 @@
 import React from 'react';
-import {createStore} from 'redux';
-import {Board} from './src/components/Board';
-import {render} from 'react-dom';
-import {reducer} from './src/store';
-import {Provider} from 'react-redux';
+//import {createStore} from 'redux';
+//import {Board} from './src/components/Board';
+//import {render} from 'react-dom';
+//import {reducer} from './src/store';
+//import {Provider} from 'react-redux';
+import {Grid} from './src/grid.js';
 
 console.log('Definitely working!');
 
 const store = createStore(reducer);
 
+
 render(
+
+    
+    <Grid/>,
+    
+    document.getElementById('app-mount')
+
+    
+
+/*
     <Provider store={store}>
         <Board/>
     </Provider>,
     document.getElementById('app-mount')
+    */
 );
---
+++ src/grid.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+var gridSize = 10;
+
+export class Grid extends React.Component{
+
+    constructor(props){
+        super(props);
+    
+    }
+
+    render() {
+    
+        return( 
+            <div>This is Where the grid will be</div>
+            
+        )
+    
+    }
+}
